f00bebd8912f147bad7656698ce084fbc2d12a0a
Eguix-2010302 <Eguix-2010302@kinal.edu.gt>
2016-09-07 09:41:39 -0600

    Script de base de datos

---
+++ descargas.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Community v12.2.1 (64 bit)
+MySQL - 5.6.17 : Database - descargas
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`descargas` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `descargas`;
+
+/*Table structure for table `informacion` */
+
+DROP TABLE IF EXISTS `informacion`;
+
+CREATE TABLE `informacion` (
+  `idPersona` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(25) DEFAULT NULL,
+  `correo` varchar(25) DEFAULT NULL,
+  `telefono` int(25) DEFAULT NULL,
+  `archivo` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`idPersona`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+/*Data for the table `informacion` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
